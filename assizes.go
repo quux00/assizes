89d0fff0f4a20b74d3d881b059bda8baa8574712
Michael <mpeterson2@gmail.com>
2013-12-01 00:52:30 -0500

    Added AssertIntSlicesNotEqual assertion.

--- assizes.go
+++ assizes.go
@@ -16,14 +16,14 @@ type Assize struct {
 func (a Assize) AssertTrue(b bool) {
 	if !b {
 		_, _, line, _ := runtime.Caller(1)
-		a.T.Errorf("assertTrue failed: line %v", line)
+		a.T.Errorf("AssertTrue failed: line %v", line)
 	}
 }
 
 func (a Assize) AssertFalse(b bool) {
 	if b {
 		_, _, line, _ := runtime.Caller(1)
-		a.T.Errorf("assertFalse failed: line %v", line)
+		a.T.Errorf("AssertFalse failed: line %v", line)
 	}
 }
 
@@ -32,7 +32,7 @@ func (a Assize) AssertEqual(exp, act interface{}) {
 	sact := fmt.Sprintf("%v", act)
 	if sexp != sact {
 		_, _, line, _ := runtime.Caller(1)
-		a.T.Errorf("assertEqual failed (line %d). exp: %s; act: %s",
+		a.T.Errorf("AssertEqual failed (line %d). exp: %s; act: %s",
 			line, sexp, sact)
 	}
 }
@@ -40,28 +40,43 @@ func (a Assize) AssertEqual(exp, act interface{}) {
 func (a Assize) AssertNotNil(act interface{}) {
 	if act == nil {
 		_, _, line, _ := runtime.Caller(1)
-		a.T.Errorf("assertNotNil failed (line %d).", line)
+		a.T.Errorf("AssertNotNil failed (line %d).", line)
 	}
 }
 
 func (a Assize) AssertNil(act interface{}) {
 	if act != nil {
 		_, _, line, _ := runtime.Caller(1)
-		a.T.Errorf("assertNil failed (line %d); act = %v", line, act)
+		a.T.Errorf("AssertNil failed (line %d); act = %v", line, act)
 	}
 }
 
 func (a Assize) AssertIntSlicesEqual(exp, act []int) {
 	_, _, line, _ := runtime.Caller(1)
 	if len(exp) != len(act) {
-		a.T.Errorf("assertIntSlicesEqual failed (line %d). Lengths differ: len(exp): %d; len(act): %d",
+		a.T.Errorf("AssertIntSlicesEqual failed (line %d). Lengths differ: len(exp): %d; len(act): %d",
 			line, len(exp), len(act))
 		return
 	}
 	for i, v := range exp {
 		if v != act[i] {
-			a.T.Errorf("assertIntSlicesEqual failed (line %d). Differ at index %d: exp[%d]: %d; act[%d]: %d",
+			a.T.Errorf("AssertIntSlicesEqual failed (line %d). Differ at index %d: exp[%d]: %d; act[%d]: %d",
 				line, i, i, v, i, act[i])
 		}
 	}
+}
+
+func (a Assize) AssertIntSlicesNotEqual(exp, act []int) {
+	if len(exp) != len(act) {
+		// if not same length, then not equal, so assertion passes; just return
+		return
+	}
+	for i, v := range exp {
+		if v != act[i] {
+			return
+		}
+	}
+	// if get here the slices have equal size and values
+	_, _, line, _ := runtime.Caller(1)
+	a.T.Errorf("AssertIntSlicesNotEqual failed (line %d). Len and values all match", line)
 }
